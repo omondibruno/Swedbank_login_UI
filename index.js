11c1d96cf89737191b8ff51dc5e33aca0b42e870
omondibruno <78784069+omondibruno@users.noreply.github.com>
2021-12-27 08:20:50 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var x = document.getElementById("tabs");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction1() {
+    var x = document.getElementById("list");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
